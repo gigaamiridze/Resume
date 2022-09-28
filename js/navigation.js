f294337559252dc1ad63328397b97ec7c8c025ef
Giga Amiridze <flameyungoxx@gmail.com>
2022-09-28 21:43:44 +0400

    Changed image addresses

--- js/navigation.js
+++ js/navigation.js
@@ -6,7 +6,7 @@ window.addEventListener("scroll", () => {
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+    if (pageYOffset >= sectionTop - sectionHeight / 5) {
       current = section.getAttribute("id");
     }
   });
